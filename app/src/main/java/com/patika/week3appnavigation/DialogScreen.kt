27ec2fa618c25e50d29e21e26d256b321da60daa
Metehan Özcan <70481060+metehanozcan@users.noreply.github.com>
2022-09-10 22:26:14 +0300

    Login screen button layout implementation

---
+++ app/src/main/java/com/patika/week3appnavigation/DialogScreen.kt
@@ -0,0 +1,27 @@
+package com.patika.week3appnavigation
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+import androidx.navigation.fragment.findNavController
+
+class DialogScreen : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            val builder = AlertDialog.Builder(it)
+            builder.setMessage("You are about to login")
+                .setPositiveButton("OK") { dialog, id ->
+                    findNavController().apply {
+                        navigate(R.id.action_dialogScreen_to_homeActivity)
+
+                        //code for cleaning all backstack
+                        activity?.finishAffinity()
+                    }
+                }
+                .setNegativeButton("Cancel", { dialog, id ->
+                })
+            builder.create()
+        } ?: throw IllegalStateException("activity can not be null")
+    }
+}
---
+++ app/src/main/java/com/patika/week3appnavigation/LoginFragment.kt
@@ -0,0 +1,35 @@
+package com.patika.week3appnavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.patika.week3appnavigation.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+
+    private lateinit var fragmentLoginBinding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        //fragmentLoginBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_login,container,false)
+        fragmentLoginBinding = FragmentLoginBinding.inflate(layoutInflater)
+        return fragmentLoginBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fragmentLoginBinding.apply {
+            buttonLogin.setOnClickListener {
+                findNavController().navigate(R.id.action_loginFragment_to_authenticationTwo)
+            }
+        }
+    }
+}
